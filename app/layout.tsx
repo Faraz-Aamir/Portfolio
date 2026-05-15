023f6d30b9c041449e948b2a92e4f17ab057aa78
faraz aamir <farazaamir126@gamil.com>
2026-05-15 23:50:13 +0500

    Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
